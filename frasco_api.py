8b933acf68cceb0a2b3529331cbc461487991038
maximebf <maxime.bouroumeau@gmail.com>
2016-05-18 12:44:39 +0200

    added build of swagger client

--- frasco_api.py
+++ frasco_api.py
@@ -1,10 +1,13 @@
-from frasco import Feature, Service, action, hook, abort, current_app, request, jsonify, g, Blueprint
+from frasco import Feature, Service, action, hook, abort, current_app, request, jsonify, g, Blueprint, url_for, signal
 from frasco_models import as_transaction, transaction, save_model
 import datetime
 import base64
 import hashlib
 import uuid
 import re
+import os
+import json
+import codecs
 from apispec import APISpec
 from flask_cors import CORS
 
@@ -46,9 +49,18 @@ class ApiFeature(Feature):
                 "cors_resources": {},
                 "cors_allow_services": True,
                 "spec_title": "API",
-                "spec_version": "1.0"}
+                "spec_version": "1.0",
+                "swagger_client_var_name": "API",
+                "swagger_client_class_name": "SwaggerClient",
+                "swagger_client_filename": None,
+                "swagger_client_cdn_version": "master",
+                "static_dir": None,
+                "auto_assets": False}
 
     def init_app(self, app):
+        if not self.options["static_dir"]:
+            self.options["static_dir"] = app.static_folder
+
         if self.options['allow_cross_requests']:
             resources = dict(**self.options['cors_resources'])
             if self.options['cors_allow_services']:
@@ -82,11 +94,17 @@ class ApiFeature(Feature):
                     save_model(key)
                 return key.user
 
-        spec_bp = Blueprint("apispec", __name__)
-        @spec_bp.route("/spec.json")
-        def get_spec():
-            return jsonify(**self.build_spec().to_dict())
-        app.register_service_blueprint(spec_bp)
+        self.register_spec_blueprint(app)
+        if "assets" in app.features:
+            app.assets.register({
+                "swagger-client-cdn": [
+                    "https://cdn.rawgit.com/swagger-api/swagger-js/%s/browser/swagger-client.min.js" % self.options["swagger_client_cdn_version"]]})
+            if self.options['auto_assets']:
+                app.features.assets.add_default("@swagger-client-cdn")
+            if self.options["swagger_client_filename"]:
+                app.features.assets.register_assets_builder(self.write_swagger_client)
+                if self.options['auto_assets']:
+                    app.features.assets.add_default(self.options["swagger_client_filename"])
 
     @action('create_api_key', default_option='user', as_='api_key')
     @as_transaction
@@ -100,6 +118,24 @@ class ApiFeature(Feature):
         key.expires_at = expires_at
         save_model(key)
         return key
+
+    def register_spec_blueprint(self, app):
+        spec_bp = Blueprint("apispec", __name__)
+        cache = {}
+
+        @spec_bp.route("/spec.json")
+        def get_spec():
+            if "spec" not in cache:
+                cache["spec"] = self.build_spec().to_dict()
+            return jsonify(**cache["spec"])
+
+        @spec_bp.route("/swagger.js")
+        def get_swagger_client():
+            if "client" not in cache:
+                cache["client"] = self.build_swagger_client()
+            return cache["client"], 200, {'Content-Type': 'application/javascript'}
+
+        app.register_service_blueprint(spec_bp)
 
     def build_spec(self):
         spec = APISpec(title=self.options['spec_title'],
@@ -148,3 +184,18 @@ class ApiFeature(Feature):
                         o['description'] = p.help
                     params.append(o)
         return params
+
+    def build_swagger_client(self):
+        return "function %s(options) { options['spec'] = %s; %s.call(this, '%s', options); } %s.prototype = new %s();" % (
+            self.options['swagger_client_var_name'],
+            json.dumps(self.build_spec().to_dict()),
+            self.options['swagger_client_class_name'],
+            url_for('apispec.get_spec', _external=True),
+            self.options['swagger_client_var_name'],
+            self.options['swagger_client_class_name']
+        )
+
+    def write_swagger_client(self):
+        filename = os.path.join(self.options["static_dir"], self.options["swagger_client_filename"])
+        with codecs.open(filename, "w", "utf-8") as f:
+            f.write(self.build_swagger_client())
